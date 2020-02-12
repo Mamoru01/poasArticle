f2601b6dcaf5dffc6c65de5916135b7e3025252e
Alexandr <zubkov.alexander.v@gmail.com>
2020-02-12 10:59:31 +0400

    deleted useless scripts

--- public/models/AuthorsArticle.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = require('./db');
-
-module.exports = AuthorsArticle = sequelize.define('Articles_has_Authors', {
-    firstAuthor: Sequelize.BOOLEAN
-}, { timestamps: false });
